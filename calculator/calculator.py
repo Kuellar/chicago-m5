b06d70d6e05e06c2e0096f4ad2b876c10af0a1ce
Kuellar <icuellartapia@gmail.com>
2022-11-16 20:19:22 -0300

    First commit

---
+++ calculator/calculator.py
@@ -0,0 +1,15 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+
+def add(x, y):
+    return x + y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def subtract(x, y):
+    return x - y
