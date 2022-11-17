43f33dec9b34c573b57f9db13ebda75c0ad1a88e
Kuellar <icuellartapia@gmail.com>
2022-11-16 22:03:53 -0300

    Add test

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -16,3 +16,7 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_divition(self):
+        assert 2 == calculator.divide(10, 5)
+
